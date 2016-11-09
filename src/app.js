aec0add9cdac0917fda1e3aa1c21cfd73d2ca576
Adrián Jiménez Villarreal <UO227602@uniovi.es>
2016-11-09 12:08:29 +0100

    Tira la torre acabado basico

--- src/app.js
+++ src/app.js
@@ -1,10 +1,57 @@
+var tipoMuro = 2;
+var tipoBloque = 3;
+
 
 var GameLayer = cc.Layer.extend({
+    mundoActivo: false,
+    space:null,
+    spritePelota:null,
     arrayBloques:[],
+    formasEliminar: [],
     spriteFondo: null,
+    tiempo:0,
     ctor:function () {
         this._super();
         var size = cc.winSize;
+
+        // Inicializar Space
+        this.space = new cp.Space();
+        this.space.gravity = cp.v(0, -350);
+
+        this.depuracion = new cc.PhysicsDebugNode(this.space);
+        this.addChild(this.depuracion, 10);
+
+        // Muros
+        var muroIzquierdaShape = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, 0),// Punto de Inicio
+            cp.v(0, size.height),// Punto final
+            10);// Ancho del muro
+        this.space.addStaticShape(muroIzquierdaShape);
+
+        var muroArribaShape = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, size.height),// Punto de Inicio
+            cp.v(size.width, size.height),// Punto final
+            10);// Ancho del muro
+        this.space.addStaticShape(muroArribaShape);
+
+        var muroDerechaShape = new cp.SegmentShape(this.space.staticBody,
+            cp.v(size.width, 0),// Punto de Inicio
+            cp.v(size.width, size.height),// Punto final
+            10);// Ancho del muro
+        this.space.addStaticShape(muroDerechaShape);
+
+        var muroAbajoShape = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, 0),// Punto de Inicio
+            cp.v(size.width, 0),// Punto final
+            10);// Ancho del muro
+            muroAbajoShape.setFriction(1);
+            muroAbajoShape.setCollisionType(tipoMuro);
+        this.space.addStaticShape(muroAbajoShape);
+
+        // muro y bloque
+        this.space.addCollisionHandler(tipoMuro, tipoBloque,
+                         null, null, this.collisionBloqueConMuro.bind(this), null);
+
 
 
         // Fondo
@@ -13,11 +60,35 @@ var GameLayer = cc.Layer.extend({
         this.spriteFondo.setScale( size.width / this.spriteFondo.width );
         this.addChild(this.spriteFondo);
 
+        // cache
+        cc.spriteFrameCache.addSpriteFrames(res.animacion_bola_plist);
+        cc.spriteFrameCache.addSpriteFrames(res.barra_3_plist);
+        cc.spriteFrameCache.addSpriteFrames(res.animacioncocodrilo_plist);
+
+        // Sprite pelota
+        this.spritePelota = new cc.PhysicsSprite("#animacion_bola1.png");
+
+        var body = new cp.Body(1, cp.momentForCircle(1,0,this.spritePelota.width/2, cp.vzero));
+
+        body.p = cc.p(size.width*0.1 , size.height*0.5);
+
+        this.spritePelota.setBody(body);
+        //Agregar el body al spacio sino no funciona
+        this.space.addBody(body);
+
+        var shape = new cp.CircleShape(body, this.spritePelota.width/2, cp.vzero);
+        this.space.addShape(shape);
+        this.addChild(this.spritePelota);
+
+
         // Evento MOUSE
         cc.eventManager.addListener({
             event: cc.EventListener.MOUSE,
             onMouseDown: this.procesarMouseDown
         }, this);
+
+        this.inicializarPlataformas();
+        this.inicializarBloques();
 
         this.scheduleUpdate();
         return true;
@@ -25,10 +96,119 @@ var GameLayer = cc.Layer.extend({
     },procesarMouseDown:function(event) {
              // Ambito procesarMouseDown
              var instancia = event.getCurrentTarget();
+              if(instancia.tiempo == 0 ){
+                 instancia.mundoActivo = true;
+                 instancia.tiempo = new Date().getTime();
+                 // PRUEBA 1.
+                 //instancia.spritePelota.body.applyImpulse(cp.v(0, 300),
+                 // cp.v(0, 0));
+
+                 // PRUEBA 2:
+                  var body = instancia.spritePelota.body;
+                  body.applyImpulse(cp.v( event.getLocationX() - body.p.x, event.getLocationY() - body.p.y), cp.v(0,0));
+              }
 
      },update:function (dt) {
 
-     }
+        if(this.mundoActivo){
+               this.space.step(dt);
+           }
+
+        for(var i = 0; i < this.formasEliminar.length; i++) {
+                var shape = this.formasEliminar[i];
+
+                for (var i = 0; i < this.arrayBloques.length; i++) {
+                  if (this.arrayBloques[i].body.shapeList[0] == shape) {
+                      // quita la forma
+                      this.space.removeShape(shape);
+                      // quita el cuerpo *opcional, funciona igual
+                      this.space.removeBody(shape.getBody());
+                      // quita el sprite
+                      this.arrayBloques[i].removeFromParent();
+                      // Borrar tambien de ArrayBloques
+                      this.arrayBloques.splice(i, 1);
+                  }
+                }
+            }
+            this.formasEliminar = [];
+
+        if( this.arrayBloques.length > 0){
+
+            var todosSinMoverse = true;
+            for(var i = 0; i < this.arrayBloques.length; i++) {
+                var velocidadBloque = this.arrayBloques[i].body.getVel();
+                if( velocidadBloque.x < -0.09 && velocidadBloque.x > 0.09 ){
+                    // NO - ESTE SE MUEVE
+                    todosSinMoverse = false;
+                }
+            }
+            if ( this.tiempo != 0 && (new Date().getTime() - this.tiempo) > 10000 && todosSinMoverse ){
+                cc.director.pause();
+                cc.audioEngine.stopMusic();
+                this.getParent().addChild(new GameOverLayer());
+            }
+
+        } else { //  arrayBloques.length == 0
+            cc.director.pause();
+            cc.audioEngine.stopMusic();
+            this.getParent().addChild(new GameOverLayer());
+        }
+
+
+     },inicializarPlataformas:function () {
+
+                     var spritePlataforma = new cc.PhysicsSprite("#barra_3.png");
+
+                     var body = new cp.StaticBody();
+                     body.p = cc.p(cc.winSize.width*0.7 , cc.winSize.height*0.4);
+                     spritePlataforma.setBody(body);
+                      // Los cuerpos estáticos no se añaden al espacio
+                     //this.space.addBody(body);
+
+                     var shape = new cp.BoxShape(body, spritePlataforma.width, spritePlataforma.height);
+                     // addStaticShape en lugar de addShape
+                     shape.setFriction(1);
+                     this.space.addStaticShape(shape);
+
+                     this.addChild(spritePlataforma);
+
+     },inicializarBloques:function () {
+                    var altoTorre = 0;
+
+                    while(altoTorre < 4){
+
+                         var spriteBloque = new cc.PhysicsSprite("#cocodrilo1.png");
+
+                         // Masa 1
+                         var body = new cp.Body(1, cp.momentForBox(1, spriteBloque.width, spriteBloque.height));
+
+                         body.p = cc.p(cc.winSize.width*0.7 , cc.winSize.height*0.4 + 10 + 20 + spriteBloque.height*altoTorre);
+
+                         spriteBloque.setBody(body);
+                         // Este si hay que añadirlo
+                         this.space.addBody(body);
+
+                         var shape = new cp.BoxShape(body, spriteBloque.width, spriteBloque.height);
+                         shape.setFriction(1);
+                         shape.setCollisionType(tipoBloque);
+                         this.space.addShape(shape);
+                         this.addChild(spriteBloque);
+
+                         // agregar el Sprite al array Bloques
+                         this.arrayBloques.push(spriteBloque);
+
+                         altoTorre++;
+
+                   }
+
+        },collisionBloqueConMuro:function (arbiter, space) {
+                    var shapes = arbiter.getShapes();
+                    // shapes[0] es el muro
+                    this.formasEliminar.push(shapes[1]);
+          }
+
+
+
 });
 
 var GameScene = cc.Scene.extend({
